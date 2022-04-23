361f3353bbd0942335ec4ebe8574c257d0720d51
Benjamin Avila <developerbenjaminar@gmail.com>
2022-04-23 12:13:31 -0500

    Cambio setiing.py agrego variables de entono y utilizo decouple

--- amsa/settings.py
+++ amsa/settings.py
@@ -12,19 +12,16 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2MDXiKOA3UcPKkVB)Tr8R>h)VuJZQ<IDWW*2L4u05PRf!MmkddvE9NLIO7aq#ERBsqCzhN(sO97nXo)St9My!4MIBx2o3KOJcnekd72A#K#prl'
-#SECRET_KEY = 'django-insecure-!68t3c7!%4uc+0jtaq@o&=#$=-5!-5y6_tx$jkw#r=_yg2)5t3'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+SECRET_KEY = config('SECRET_KEY')
+
 DEBUG = True
 
 ALLOWED_HOSTS = []
@@ -38,7 +35,6 @@ DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 '''
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -80,10 +76,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'amsa.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -94,10 +86,6 @@ DATABASES = {
         'PORT': 5432,
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -114,13 +102,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/3.2/topics/i18n/
-
 LANGUAGE_CODE = 'es-mx'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC-5'
 
 USE_I18N = True
 
@@ -129,19 +113,12 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
 LOGIN_REDIRECT_URL = '/factura/index'
 LOGOUT_REDIRECT_URL ='/login'
 LOGIN_URL='/login'
