cb5f8684c9e3a5b2a5fcf7ea026be0b0e3b08869
Benjamin Avila <developerbenjaminar@gmail.com>
2022-04-29 01:59:42 -0500

    Modificacion de views app_autenticacion de funciones a clases, estilos de formularios

--- apps/app_autenticacion/urls.py
+++ apps/app_autenticacion/urls.py
@@ -1,9 +1,14 @@
-from apps.app_autenticacion import views
+#Para vista basada em funciones
+#from apps.app_autenticacion import views
 from django.urls import  path
+from .views import Registro
 
 
 urlpatterns = [
-    path('', views.autenticacion, name='Autenticacion'),
+    #path para vistas basadas en clases.
+    path('registro/',Registro.as_view(), name='autenticacion')
+    #path para vista basadas en fucniones.
+    #path('', views.autenticacion, name='Autenticacion'), 
 ]
 
 
--- apps/app_autenticacion/views.py
+++ apps/app_autenticacion/views.py
@@ -1,15 +1,32 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
-
+from django.views.generic import View
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
+class Registro(View):
+    
+    def get(self, request):
+        form = UserCreationForm()
+        return render(request, "autenticacion/registro.html",{"form":form})
+    
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid:
+            form.save()
+        else:
+            return redirect('/login/')
+
+
+
+'''
 def autenticacion(request):
     if request.session.session_key:
         return render(request, "autenticacion/registro.html")
     else:
         return redirect('/login/')
-
+'''
 '''
 def base(request):
     if  request.session.session_key:
