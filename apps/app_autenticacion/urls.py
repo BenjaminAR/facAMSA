c0f62ff81f576209ce30f816500926cd87a009c7
Benjamin Avila <developerbenjaminar@gmail.com>
2022-04-29 18:18:06 -0500

    Cmabios en app_autenticacion

--- apps/app_autenticacion/urls.py
+++ apps/app_autenticacion/urls.py
@@ -1,12 +1,13 @@
 #Para vista basada em funciones
 #from apps.app_autenticacion import views
 from django.urls import  path
-from .views import Registro
+from .views import Registro, RegistroAll
 
 
 urlpatterns = [
     #path para vistas basadas en clases.
-    path('registro/',Registro.as_view(), name='autenticacion')
+    path('registro/',Registro.as_view(), name='registro'),
+    path('registroall/', RegistroAll.as_view(), name='registroall'),
     #path para vista basadas en fucniones.
     #path('', views.autenticacion, name='Autenticacion'), 
 ]
--- apps/app_autenticacion/views.py
+++ apps/app_autenticacion/views.py
@@ -1,36 +1,39 @@
+import re
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from django.views.generic import View
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth import login, logout
 # Create your views here.
 
 class Registro(View):
     
     def get(self, request):
         form = UserCreationForm()
-        return render(request, "autenticacion/registro.html",{"form":form})
+        return render(request, "autenticacion/registro.html", {"form":form})
     
     def post(self, request):
         form = UserCreationForm(request.POST)
         if form.is_valid:
-            form.save()
+            usuario = form.save()
+            login(request, usuario)
+            return redirect('/log/registroall/')
         else:
+            logout(request, usuario)
             return redirect('/login/')
 
+class RegistroAll(View):
 
+    def get(self, request):
+        form = UserChangeForm()
+        return render(request, "autenticacion/registroall.html", {"form":form})
 
-'''
-def autenticacion(request):
-    if request.session.session_key:
-        return render(request, "autenticacion/registro.html")
-    else:
-        return redirect('/login/')
-'''
-'''
-def base(request):
-    if  request.session.session_key:
-        return redirect('/factura/index')
-    else:
-        return redirect('/login/')
-'''
+    def post(self, request):
+        form = UserChangeForm(request.POST)
+        if form.is_valid:
+            form.save()
+            return redirect('/login/')
+        else:
+            form = UserChangeForm()
+            return render( request, "autenticacion/registroall.html", {"form":form})
