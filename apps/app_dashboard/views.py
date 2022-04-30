1822b1a7c668328e401214d21661aa5a3152cd3f
Benjamin Avila <developerbenjaminar@gmail.com>
2022-04-29 19:44:26 -0500

    Cambios en app_dashboard, if else

--- apps/app_dashboard/views.py
+++ apps/app_dashboard/views.py
@@ -1,12 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+#from apps.app_factura.models import Solicitud, Solicitud_atendida
 
 # Create your views here.
 
 
 class Dashboard(View):
     def get(self, request):
-        return render(request, "dashboard/dashboard.html")
+        if request.session.session_key:
+            print(request.session.session_key)
+            return render(request, "dashboard/dashboard.html")
+            
+        else: 
+            return redirect('/login/')
 
     def post(self, request):
         pass
+
+
+
