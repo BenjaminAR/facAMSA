4aac1625e74736333b90d8f2861f5411f66fa3a4
Benjamin Avila <developerbenjaminar@gmail.com>
2022-05-07 14:05:12 -0500

    Se agrega variable para manejar archivos media en debug

--- amsa/settings.py
+++ amsa/settings.py
@@ -107,7 +107,8 @@ USE_L10N = True
 
 
 STATIC_URL = '/static/'
-
+STATIC_MEDIA ='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media'),
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
@@ -115,7 +116,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/factura/index'
 LOGOUT_REDIRECT_URL ='/login'
-LOGIN_URL='/login'
+LOGIN_URL='/login' 
 
 MESSAGAE_TAGS={
 
